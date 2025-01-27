1fd54db3fdb266c957d9e6b963aa782490042eca
Isu <ntrojak98@gmail.com>
2025-01-27 18:09:34 +0100

    Added and changed queries and schemast to follow feeds

--- sql/queries/feedFollows.sql
+++ sql/queries/feedFollows.sql
@@ -1,10 +1,10 @@
--- Name: CreateFeedFollow :one
-WITH createdFeedFollow  (
+-- name: CreateFeedFollow :one
+WITH created_feed_follow AS (
     INSERT INTO feed_follows (id, created_at, updated_at, user_id, feed_id)
-    VALUES ($1, $2, $3, $4, $5);
-    RETURNING *;
+    VALUES ($1, $2, $3, $4, $5)
+    RETURNING *
     )
-SELECT createdFeedFollow, users.name, feeds.name 
-FROM feeds
-JOIN users ON users.id = feeds.user_id
-WHERE feeds.id = createdFeedFollow.feed_id;
+SELECT created_feed_follow.*, users.name AS user_name, feeds.name AS feed_name
+FROM created_feed_follow
+JOIN users ON users.id = created_feed_follow.user_id
+JOIN feeds ON feeds.id = created_feed_follow.feed_id;
